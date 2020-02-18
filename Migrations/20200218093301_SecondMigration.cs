27566d6d47ce5f3fa0fa1bd0a42949fac841df69
Jan Eickmans <jan.eickmans@hotmail.nl>
2020-02-18 11:26:51 +0100

    Pushing project to git

---
+++ Migrations/20200218093301_SecondMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunWebAPI.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunWebAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+    }
+}
