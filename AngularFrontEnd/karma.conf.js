5eb24c16b0a75caad913ac3006bd0c41d8629d69
Jan Eickmans <jan.eickmans@hotmail.nl>
2020-03-02 21:56:09 +0100

    Remade angular pages folder to allow a fresh restart

--- AngularFrontEnd/karma.conf.js
+++ AngularFrontEnd/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularPages'),
+      dir: require('path').join(__dirname, './coverage/AngularFrontEnd'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
