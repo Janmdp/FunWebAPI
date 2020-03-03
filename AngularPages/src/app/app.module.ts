5502f04c35b6e32163c27a56c6e76887d79391d0
Jan Eickmans <i400058@fhict.nl>
2020-03-03 08:40:00 +0100

    Revert "Remade angular pages folder to allow a fresh restart"
    This reverts commit 5eb24c16b0a75caad913ac3006bd0c41d8629d69.

--- AngularPages/src/app/app.module.ts
+++ AngularPages/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserService } from './shared/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
